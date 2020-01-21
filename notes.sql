68d28d99bf36404cff5b70c56e0ffbb4345554ed
Wesley Berry <wesleyberry52@gmail.com>
2020-01-21 15:32:35 -0600

    deleted from table

--- notes.sql
+++ notes.sql
@@ -3,7 +3,7 @@ CREATE DATABASE students_db;
 USE students_db;
 
 CREATE TABLE student (
-	student_id INT,
+	student_id INT AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
     major VARCHAR(20),
     PRIMARY KEY(student_id)
@@ -16,9 +16,14 @@ DROP TABLE student;
 ALTER TABLE student ADD gpa DECIMAL(3, 2);
 ALTER TABLE student DROP COLUMN gpa;
 
-INSERT INTO student VALUES (1, 'Jack', 'Biology');
-INSERT INTO student VALUES (2, 'Kate', 'Sociology');
-INSERT INTO student(student_id, name) VALUES (3, 'Claire');
-INSERT INTO student VALUES (4, 'Jack', 'Biology');
-INSERT INTO student VALUES (5, 'Mike', 'Computer Science');
+INSERT INTO student(name, major) VALUES ('Jack', 'Biology');
+INSERT INTO student(name, major) VALUES ('Kate', 'Sociology');
+INSERT INTO student(name) VALUES ('Claire');
+INSERT INTO student(name, major) VALUES ('Jack', 'Biology');
+INSERT INTO student(name, major) VALUES ('Mike', 'Computer Science');
 SELECT * FROM student;
+
+UPDATE student SET major = 'Bio' WHERE major = 'Biology';
+UPDATE student SET major = 'comp sci' WHERE major = 'Computer Science';
+
+DELETE FROM student WHERE student_id = 1;
