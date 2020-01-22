7a8ef2ce847be189a66bf4d296652594e51a2fed
Wesley Berry <wesleyberry52@gmail.com>
2020-01-22 13:45:53 -0600

    added to employee table

--- company.sql
+++ company.sql
@@ -50,3 +50,11 @@ CREATE TABLE branch_supplier(
     FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
 );
 DESCRIBE branch_supplier;
+
+INSERT INTO employee VALUES(100, 'David', 'Wallace', '1967-11-17', 'M', 250000, NULL, NULL);
+INSERT INTO branch VALUES(1, 'Corporate', 100, '2006-02-09');
+UPDATE employee SET branch_id = 1 WHERE emp_id = 100;
+
+INSERT INTO employee VALUES(106, 'Josh', 'Porter', '1969-09-05', 'M', 7800, 100, NULL);
+INSERT INTO branch VALUES(3, 'Stamford', 106, '1998-02-13');
+UPDATE employee SET branch_id = 3 WHERE emp_id = 106;
