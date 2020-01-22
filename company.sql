a43042d214fb8e61283004cfafe327e725671942
Wesley Berry <wesleyberry52@gmail.com>
2020-01-22 13:32:05 -0600

    created branch_supplies table

--- company.sql
+++ company.sql
@@ -41,3 +41,12 @@ CREATE TABLE works_with(
     FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
 );
 DESCRIBE works_with;
+
+CREATE TABLE branch_supplier(
+	branch_id INT,
+    supplier_name VARCHAR(40),
+    supply_type VARCHAR(40),
+    PRIMARY KEY(branch_id, supplier_name),
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
+);
+DESCRIBE branch_supplier;
