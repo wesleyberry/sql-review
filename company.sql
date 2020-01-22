0ea2ea6db9eaa207a45878cd8b28b5273841dcc8
Wesley Berry <wesleyberry52@gmail.com>
2020-01-22 13:29:05 -0600

    created works_with table

--- company.sql
+++ company.sql
@@ -23,3 +23,21 @@ CREATE TABLE branch (
 DESCRIBE branch;
 ALTER TABLE employee ADD FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL;
 ALTER TABLE employee ADD FOREIGN KEY(super_id) REFERENCES employee(emp_id) ON DELETE SET NULL;
+
+CREATE TABLE client(
+	client_id INT PRIMARY KEY,
+    client_name VARCHAR(40),
+    branch_id INT,
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL
+);
+DESCRIBE client;
+
+CREATE TABLE works_with(
+	emp_id INT,
+    client_id INT,
+    total_sales INT,
+    PRIMARY KEY(emp_id, client_id),
+    FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
+    FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
+DESCRIBE works_with;
