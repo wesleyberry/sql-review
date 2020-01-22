60a1de49f5678ece1add8618e20cdc3be785528b
Wesley Berry <wesleyberry52@gmail.com>
2020-01-22 13:18:03 -0600

    created table for employee

---
+++ company.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
+
+CREATE TABLE employee (
+	emp_id INT PRIMARY KEY,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    birth_dat DATE,
+    sex VARCHAR(1),
+    salary INT,
+    super_id INT,
+    branch_id INT
+);
